289d9dda23470942db8634c6c604e559ed9f0f3c
GreenLight <1251191746@qq.com>
2017-06-07 21:08:19 +0800

    “查询功能实现

--- src/com/gb/util/Query.java
+++ src/com/gb/util/Query.java
@@ -27,10 +27,10 @@ public class Query {
             BufferedReader br2 = new BufferedReader(new FileReader(id2nameFile));
             BufferedReader br3 = new BufferedReader(new FileReader(id2allFile));
             BufferedReader br4 = new BufferedReader(new FileReader(id2propertyFile));
-            bw1 = new BufferedWriter(new FileWriter(name2idFile,true));
-            bw2 = new BufferedWriter(new FileWriter(id2nameFile,true));
-            bw3 = new BufferedWriter(new FileWriter(id2allFile,true));
-            bw4 = new BufferedWriter(new FileWriter(id2propertyFile,true));
+            bw1 = new BufferedWriter(new FileWriter(name2idFile, true));
+            bw2 = new BufferedWriter(new FileWriter(id2nameFile, true));
+            bw3 = new BufferedWriter(new FileWriter(id2allFile, true));
+            bw4 = new BufferedWriter(new FileWriter(id2propertyFile, true));
             name2id = new Properties();
             id2name = new Properties();
             id2all = new Properties();
@@ -46,11 +46,50 @@ public class Query {
 
     public static boolean isIdExist(int id) {
         String idString = String.format("0x%08x", id);
-        String str = id2name.getProperty(idString,"");
+        String str = id2name.getProperty(idString, "");
         return !str.isEmpty();
     }
 
-    public static boolean findName() {
-        return true;
+    /**
+     * @param name 传入要查询的名字
+     * @return 0表示没有找到, 其他数字代表具体id
+     */
+    public static int getIdByName(String name) {
+        String str = name2id.getProperty(name, "");
+        str = str.substring(2);
+        if (str.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(str, 16);
+    }
+
+
+    /**
+     * @param name 传入要查询的名字
+     * @return null表示没有找到, 其他包含该对象所有的信息
+     */
+    public static String getInfoByName(String name) {
+        String str = name2id.getProperty(name, "");
+//        str = str.substring(2);
+        if (!str.isEmpty()) {
+            String idStr = id2all.getProperty(name, "");
+            if (!str.isEmpty()) {
+                return str;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param id 传入要查询的名字
+     * @return null表示没有找到, 其他包含该对象所有的信息
+     */
+    public static String getInfoById(int id) {
+        String idString = String.format("0x%08x", id);
+        String str = id2all.getProperty(idString, "");
+        if (!str.isEmpty()) {
+            return str;
+        }
+        return null;
     }
 }

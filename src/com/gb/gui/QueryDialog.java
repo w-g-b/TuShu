360715e22756c7237e6f06c99a0131492980ebb4
GreenLight <1251191746@qq.com>
2017-06-11 10:49:45 +0800

    店名搜索界面

--- src/com/gb/gui/QueryDialog.java
+++ src/com/gb/gui/QueryDialog.java
@@ -3,6 +3,8 @@ package com.gb.gui;
 import com.gb.object.SpecificShop;
 import com.gb.util.Query;
 
+import javafx.scene.control.Button;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
@@ -12,138 +14,196 @@ import java.util.ArrayList;
 
 public class QueryDialog extends JDialog {
 
-    private final JPanel contentPanel = new JPanel();
-    private JTextField textField;
-    private Query query;
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private Query query;
+	private JTextField textField_1;
 
-    /**
-     * Launch the application.
-     */
-    /**
-     * Create the dialog.
-     */
-    //public QueryDialog() {}
-    public QueryDialog(JFrame f, String s, boolean b) {
-        super(f, s, b);
-        query = new Query();
-        setBounds(500, 300, 365, 485);
-        getContentPane().setLayout(new BorderLayout());
-        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-        getContentPane().add(contentPanel, BorderLayout.CENTER);
-        contentPanel.setLayout(null);
+	/**
+	 * Launch the application.
+	 */
+	/**
+	 * Create the dialog.
+	 */
+	// public QueryDialog() {}
+	public QueryDialog(JFrame f, String s, boolean b) {
+		super(f, s, b);
+		query = new Query();
+		setBounds(500, 300, 365, 485);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
 
-        JSplitPane splitPane = new JSplitPane();
-        splitPane.setBounds(47, 29, 138, 25);
-        contentPanel.add(splitPane);
+		ButtonGroup buttonGroup = new ButtonGroup();
 
-        JLabel label = new JLabel("\u8BF7\u8F93\u5165\u7AD9\u70B9\u540D\u79F0:");
-        label.setBounds(28, 83, 107, 15);
-        contentPanel.add(label);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 38, 349, 408);
+		contentPanel.add(panel_1);
+		panel_1.setLayout(null);
 
-        textField = new JTextField();
-        textField.setBounds(157, 80, 87, 21);
-        contentPanel.add(textField);
-        textField.setColumns(10);
+		JLabel lblNewLabel = new JLabel("\u8BF7\u8F93\u5165\u5E97\u540D:");
+		lblNewLabel.setBounds(38, 27, 66, 15);
+		panel_1.add(lblNewLabel);
 
-        JCheckBox checkBox = new JCheckBox("\u9152\u5E97");
-        checkBox.setBounds(74, 118, 60, 23);
-        contentPanel.add(checkBox);
+		textField_1 = new JTextField();
+		textField_1.setBounds(128, 24, 137, 21);
+		panel_1.add(textField_1);
+		textField_1.setColumns(10);
 
-        JCheckBox checkBox_1 = new JCheckBox("\u8D85\u5E02");
-        checkBox_1.setBounds(136, 118, 60, 23);
-        contentPanel.add(checkBox_1);
+		JButton button_1 = new JButton("\u5F00\u59CB\u67E5\u8BE2");
+		button_1.setBounds(120, 99, 93, 23);
+		panel_1.add(button_1);
 
-        JCheckBox checkBox_2 = new JCheckBox("\u7F8E\u98DF");
-        checkBox_2.setBounds(198, 118, 54, 23);
-        contentPanel.add(checkBox_2);
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(69, 165, 220, 214);
+		panel_1.add(scrollPane_1);
 
-        JLabel label_1 = new JLabel("\u7C7B\u578B:");
-        label_1.setBounds(28, 122, 54, 15);
-        contentPanel.add(label_1);
+		JTextArea textArea = new JTextArea();
+		scrollPane_1.setViewportView(textArea);
 
-        JLabel label_2 = new JLabel("\u6761\u4EF6:");
-        label_2.setBounds(28, 165, 54, 15);
-        contentPanel.add(label_2);
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 38, 349, 408);
+		contentPanel.add(panel);
+		panel.setLayout(null);
 
-        ButtonGroup buttonGroup = new ButtonGroup();
-        JRadioButton radioButton = new JRadioButton("\u6240\u6709");
-        radioButton.setBounds(74, 161, 65, 23);
-        contentPanel.add(radioButton);
+		JLabel label = new JLabel("\u8BF7\u8F93\u5165\u7AD9\u70B9\u540D\u79F0:");
+		label.setBounds(10, 13, 107, 15);
+		panel.add(label);
 
-        JRadioButton radioButton_1 = new JRadioButton("\u8DDD\u79BB\u6700\u8FD1");
-        radioButton_1.setBounds(136, 161, 87, 23);
-        contentPanel.add(radioButton_1);
+		textField = new JTextField();
+		textField.setBounds(142, 10, 87, 21);
+		panel.add(textField);
+		textField.setColumns(10);
 
-        JRadioButton radioButton_2 = new JRadioButton("\u8BC4\u4EF7\u6700\u597D");
-        radioButton_2.setBounds(74, 186, 121, 23);
-        contentPanel.add(radioButton_2);
+		JCheckBox checkBox = new JCheckBox("\u9152\u5E97");
+		checkBox.setBounds(70, 44, 60, 23);
+		panel.add(checkBox);
 
-        buttonGroup.add(radioButton);
-        buttonGroup.add(radioButton_1);
-        buttonGroup.add(radioButton_2);
-        radioButton.setSelected(true);
+		JCheckBox checkBox_1 = new JCheckBox("\u8D85\u5E02");
+		checkBox_1.setBounds(142, 44, 60, 23);
+		panel.add(checkBox_1);
 
-        JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setBounds(32, 246, 263, 182);
-        contentPanel.add(scrollPane);
+		JCheckBox checkBox_2 = new JCheckBox("\u7F8E\u98DF");
+		checkBox_2.setBounds(204, 44, 54, 23);
+		panel.add(checkBox_2);
 
-        JTextArea txtrFdf = new JTextArea();
-        txtrFdf.setEditable(false);
-        scrollPane.setViewportView(txtrFdf);
+		JLabel label_1 = new JLabel("\u7C7B\u578B:");
+		label_1.setBounds(35, 48, 54, 15);
+		panel.add(label_1);
 
-        JButton button = new JButton("\u5F00\u59CB\u67E5\u8BE2");
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String shopType = "(";
-                if (checkBox.isSelected()) {
-                    shopType += "[0][1]|";
-                }
-                if (checkBox_1.isSelected()) {
-                    shopType += "[0][2]|";
-                }
-                if (checkBox_2.isSelected()) {
-                    shopType += "[0][3]|";
-                }
-                shopType += "[0][0])";
-                txtrFdf.setText("");
-                String stationName = textField.getText();
-                int id = query.getStationIdByName(stationName);
-                //Query3.getInfosByid(id);
-                ArrayList<String> inforList = query.getShopInfosByStationId(id, shopType);//Query3.getInfosByName(stationName);
-                SpecificShop shops[] = new SpecificShop[inforList.size()];
-                int i = 0;
-                for (String info : inforList) {
-                    shops[i] = new SpecificShop(info);
-                    i++;
-                }
-                if (radioButton.isSelected()) {
-                    for (SpecificShop shop : shops) {
-                        txtrFdf.append(shop.toShow());
-                    }
-                }
-                if (radioButton_1.isSelected()) {
-                    SpecificShop shop = shops[0];
-                    for (int j = 1; j < shops.length; j++) {
-                        if (shop.getDistanceToStation() > shops[j].getDistanceToStation()) {
-                            shop = shops[j];
-                        }
-                    }
-                    txtrFdf.append(shop.toShow());
-                }
-                if (radioButton_2.isSelected()) {
-                    SpecificShop shop = shops[0];
-                    for (int j = 1; j < shops.length; j++) {
-                        if (shop.getRemarkGrade() < shops[j].getRemarkGrade()) {
-                            shop = shops[j];
-                        }
-                    }
-                    txtrFdf.append(shop.toShow());
+		JLabel label_2 = new JLabel("\u6761\u4EF6:");
+		label_2.setBounds(35, 107, 54, 15);
+		panel.add(label_2);
+		JRadioButton radioButton = new JRadioButton("\u6240\u6709");
+		radioButton.setBounds(70, 103, 65, 23);
+		panel.add(radioButton);
 
-                }
-            }
-        });
-        button.setBounds(116, 213, 93, 23);
-        contentPanel.add(button);
+		buttonGroup.add(radioButton);
+		radioButton.setSelected(true);
 
-    }
+		JRadioButton radioButton_1 = new JRadioButton("\u8DDD\u79BB\u6700\u8FD1");
+		radioButton_1.setBounds(227, 103, 87, 23);
+		panel.add(radioButton_1);
+		buttonGroup.add(radioButton_1);
+
+		JRadioButton radioButton_2 = new JRadioButton("\u8BC4\u4EF7\u6700\u597D");
+		radioButton_2.setBounds(137, 103, 107, 23);
+		panel.add(radioButton_2);
+		buttonGroup.add(radioButton_2);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(40, 185, 263, 182);
+		panel.add(scrollPane);
+
+		JTextArea txtrFdf = new JTextArea();
+		scrollPane.setViewportView(txtrFdf);
+		txtrFdf.setEditable(false);
+
+		JButton button = new JButton("\u5F00\u59CB\u67E5\u8BE2");
+		button.setBounds(119, 152, 93, 23);
+		panel.add(button);
+
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setBounds(0, 0, 138, 25);
+		panel.setVisible(true);
+		panel_1.setVisible(false);
+		JButton button1=new JButton("Õ¾µã");
+		JButton button2=new JButton("µêÃû");
+		button1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				panel.setVisible(true);
+				panel_1.setVisible(false);
+				
+			}
+		});
+		button2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				panel.setVisible(false);
+				panel_1.setVisible(true);
+				
+			}
+		});
+		
+		splitPane.add(button1,JSplitPane.LEFT,1);
+		splitPane.add(button2,JSplitPane.RIGHT,2);
+		contentPanel.add(splitPane);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String shopType = "(";
+				if (checkBox.isSelected()) {
+					shopType += "[0][1]|";
+				}
+				if (checkBox_1.isSelected()) {
+					shopType += "[0][2]|";
+				}
+				if (checkBox_2.isSelected()) {
+					shopType += "[0][3]|";
+				}
+				shopType += "[0][0])";
+				txtrFdf.setText("");
+				String stationName = textField.getText();
+				int id = query.getStationIdByName(stationName);
+				// Query3.getInfosByid(id);
+				ArrayList<String> inforList = query.getShopInfosByStationId(id, shopType);// Query3.getInfosByName(stationName);
+				SpecificShop shops[] = new SpecificShop[inforList.size()];
+				int i = 0;
+				for (String info : inforList) {
+					shops[i] = new SpecificShop(info);
+					i++;
+				}
+				if (radioButton.isSelected()) {
+					for (SpecificShop shop : shops) {
+						txtrFdf.append(shop.toShow());
+					}
+				}
+				if (radioButton_1.isSelected()) {
+					SpecificShop shop = shops[0];
+					for (int j = 1; j < shops.length; j++) {
+						if (shop.getDistanceToStation() > shops[j].getDistanceToStation()) {
+							shop = shops[j];
+						}
+					}
+					txtrFdf.append(shop.toShow());
+				}
+				if (radioButton_2.isSelected()) {
+					SpecificShop shop = shops[0];
+					for (int j = 1; j < shops.length; j++) {
+						if (shop.getRemarkGrade() < shops[j].getRemarkGrade()) {
+							shop = shops[j];
+						}
+					}
+					txtrFdf.append(shop.toShow());
+
+				}
+			}
+		});
+
+	}
 }
